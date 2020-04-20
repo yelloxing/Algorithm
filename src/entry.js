23866be6314408878e0e3211cfe75513d582de18
心叶 <yelloxing@gmail.com>
2020-04-20 23:41:54 +0800

    搭建开发环境

---
+++ src/entry.js
@@ -0,0 +1,19 @@
+import iCrush from 'icrush';
+
+// 引入启动界面
+import App from './App.iCrush';
+
+// 引入基础样式
+import '@yelloxing/normalize.css';
+
+document.getElementById('root').innerHTML = "";
+
+//根对象
+window.icrush = new iCrush({
+
+    //挂载点
+    el: document.getElementById('root'),
+
+    // 启动iCrush
+    render: createElement => createElement(App)
+});
---
+++ src/lazy-load.js
@@ -0,0 +1,4 @@
+export default {
+    "menu": () => import("./menu.iCrush"),
+    "QuickSort": () => import("./pages/QuickSort.iCrush")
+};
